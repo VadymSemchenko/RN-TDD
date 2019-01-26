28c713737366d369f00c561e101aea7ef161a031
Vadym Semchenko <v.m.semchenko.kh.ua@gmail.com>
2019-01-26 23:35:27 +0200

    added typescript dependencies

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>TDD</Text>
+      </View>
+    );
+  }
+}
