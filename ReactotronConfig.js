d5cf0171ead3d12cd1ee4df3b27d2c9e6a58483f
Vadym Semchenko <v.m.semchenko.kh.ua@gmail.com>
2019-02-02 23:22:11 +0200

    corrected reactotronconfig with deviceinfo import

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,9 +1,8 @@
 import Reactotron from 'reactotron-react-native';
 let Tron;
-let DeviceInfo;
+import DeviceInfo from 'react-native-device-info';
 
 if (__DEV__) {
-  DeviceInfo = require('react-native-device-info');
   Tron = Reactotron.configure(DeviceInfo.isEmulator() ? {} : { host: '192.168.2.10' })
     .useReactNative()
     .connect();
